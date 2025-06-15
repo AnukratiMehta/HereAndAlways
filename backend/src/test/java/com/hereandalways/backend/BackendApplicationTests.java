d172bfb5337836a890dd640a11983990d6e78c03
Anukrati Mehta <anukratimehta@gmail.com>
2025-06-15 18:44:52 +0100

    Add SecurityConfig

--- backend/src/test/java/com/hereandalways/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/hereandalways/backend/BackendApplicationTests.java
@@ -3,7 +3,10 @@ package com.hereandalways.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.hereandalways.BackendApplication;
+import com.hereandalways.config.SecurityConfig;
+
+@SpringBootTest(classes = {BackendApplication.class, SecurityConfig.class})
 class BackendApplicationTests {
 
   @Test
